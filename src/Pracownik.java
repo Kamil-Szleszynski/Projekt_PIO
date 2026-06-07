e7005d7b45aa384bd6a556b87ad4c13aab2497b9
Kamil Szleszynski <255709@edu.p.lodz.pl>
2026-06-07 16:28:35 +0200

    Stworzenie konstruktora klasy Pracownik

--- src/Pracownik.java
+++ src/Pracownik.java
@@ -1,3 +1,19 @@
 public class Pracownik {
-
+    private String imie;
+    private String nazwisko;
+    private String id;
+    public Pracownik(String imie,String nazwisko,String id){
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.id = id;
+    }
+    public String getImie(){
+        return imie;
+    }
+    public String getNazwisko(){
+        return nazwisko;
+    }
+    public String getId(){
+        return id;
+    }
 }
