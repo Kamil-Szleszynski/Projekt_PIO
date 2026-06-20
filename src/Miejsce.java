344646cba5a625b22f92dc809092629d7674b100
Bartosz979 <bartoszwaszczuk979@gmail.com>
2026-06-20 19:37:25 +0200

    Dodanie metod do rezerwacji miejsc

--- src/Miejsce.java
+++ src/Miejsce.java
@@ -1,11 +1,27 @@
 public class Miejsce {
     private int numerMiejsca;
-
+    private boolean zajete;
+    private String rezerwacjaID;
     Miejsce(int numerMiejsca){
+        this.zajete = false;
         this.numerMiejsca = numerMiejsca;
     }
 
+
     public int getNumerMiejsca() {
         return numerMiejsca;
+    }
+
+    public boolean isZajete() {
+        return zajete;
+    }
+
+    public String getRezerwacjaID() {
+        return rezerwacjaID;
+    }
+
+    public void setRezerwacjaID(String rezerwacjaID) {
+        this.rezerwacjaID = rezerwacjaID;
+        this.zajete = true;
     }
 }
--- test/MiejsceTest.java
+++ test/MiejsceTest.java
@@ -9,4 +9,17 @@ class MiejsceTest {
 
         assertEquals(10,miejsce.getNumerMiejsca());
     }
+
+    @Test
+    void sprawdzenieGet(){
+        Miejsce miejsce = new Miejsce(12);
+        assertEquals(null,miejsce.getRezerwacjaID());
+        assertEquals(false,miejsce.isZajete());
+
+        miejsce.setRezerwacjaID("2");
+
+        assertEquals("2",miejsce.getRezerwacjaID());
+        assertEquals(true,miejsce.isZajete());
+
+    }
 }
